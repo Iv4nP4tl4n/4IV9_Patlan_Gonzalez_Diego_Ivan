a6ab7c93acc10ee333a1c4aafb76646440b96a39
Iv4nP4tl4n <diegoivanpatlang@gmail.com>
2026-03-14 18:30:58 -0600

    Primera tarea y sus evidencias

--- 03EstructuraDatos/EstructuraDatos.java
+++ 03EstructuraDatos/EstructuraDatos.java
@@ -13,7 +13,7 @@ En java:
 12. Realizar un diamante.
 13. Realizar una calculadora basica. + - / * 
 
-Tarea: que es un cry catch
+Tarea:1 al 6
 
 */
 import java.util.Scanner;
@@ -30,12 +30,12 @@ public class EstructuraDatos{
         do{ 
 
             System.out.println("Bienvenido");
-            System.out.println("1- ");
-            System.out.println("2- ");
-            System.out.println("3- ");
-            System.out.println("4- ");
-            System.out.println("5- ");
-            System.out.println("6- ");
+            System.out.println("1- Descuento por edad");
+            System.out.println("2- Decimal a binario");
+            System.out.println("3- Conversor de temperaturas (C -> F y K)");
+            System.out.println("4- Positivos y negetivos");
+            System.out.println("5- Tienda de productos");
+            System.out.println("6- Area y perimetro de las figuras");
             System.out.println("7- ");
             System.out.println("8- ");
             System.out.println("9- ");
@@ -49,9 +49,36 @@ public class EstructuraDatos{
 
             switch (opcion) {
 
+
+
+
+
                 case 1:
-                    
+                    System.out.println("Ingrese su edad:  ");
+                    int edad = entrada.nextInt();
+
+                    System.out.println("Ingrese el precio del producto:  ");
+                    double precio1 = entrada.nextDouble();
+
+                    double descuento = 0.0;
+
+                    if (edad >= 60) {
+                        descuento = 0.25;
+                    } else if (edad >= 18) {
+                        descuento = 0.20;
+                    } else {
+                        descuento = 0.10;
+                    }
+
+                     double total = precio1 - (precio1 * descuento);
+
+                    System.out.println("El descuento aplicado es: " + (descuento * 100) + "%");
+                    System.out.println("El precio final es: " + total);
                     break;
+
+
+
+
 
                 case 2:
                 
@@ -83,20 +110,213 @@ public class EstructuraDatos{
 
                     System.out.println("El numero convertido a binario es: " + guardarbinario);
                     break;
+
+
+
+
+
+
                 case 3:
+                    System.out.println("Conversor de temperaturas");
+                    System.out.println("Ingrese los grados Celsius:");
+
+                        int celcius = 0;
+                        celcius = entrada.nextInt();
+
+                        double fahrenheit = (celcius * 9 / 5) + 32;
+                        double kelvin = celcius + 273.15;
+
+                        System.out.println("Temperatura en Fahrenheit: " + fahrenheit);
+                        System.out.println("Temperatura en Kelvin: " + kelvin);
                     
                     break;
+
+
+
+
+
+
 
                 case 4:
-                    
+
+                    System.out.println("Programa para contar numeros positivos y negativos");
+                    System.out.println("Ingrese cuantos numeros desea ingresar:  ");
+
+                    int cantidad = 0;
+                    cantidad = entrada.nextInt();
+
+                    int positivos = 0;
+                    int negativos = 0;
+
+                    if(cantidad > 0){
+
+                        for(int i = 1; i <= cantidad; i++){
+                            System.out.println("Ingre un numero:   ");
+                            int numero = 0;
+                            numero = entrada.nextInt();
+
+                            if(numero > 0){
+                                positivos = positivos + 1;
+                            }else if(numero < 0){
+                                negativos = negativos + 1;
+                            }
+                        }
+
+                        System.out.println("Cantidad de numeros positivos:  " + positivos);
+                        System.out.println("Cantidad de numeros negativos:  " + negativos);
+
+                    }else{
+                        System.out.println("Ingrese solo cantidades reales:  ");
+                    }
+
                     break;
+
+
+
+
+
+
 
                 case 5:
-                    
+                    System.out.println("Bienvenido a la tienda Trikitrakatelas");
+                    System.out.println("Ingrese cuantos productos va a comprar");
+
+                    int elementosproducto = 0;
+                    elementosproducto = entrada.nextInt();
+
+                    float compra = 0;
+
+                    if(elementosproducto > 0){
+
+                        System.out.println("----- Lista de productos -----");
+
+                        for(int i = 1; i <= elementosproducto; i++){
+
+                            System.out.println("Ingresa nombre del producto: ");
+                            String nombreproducto = "";
+                            nombreproducto = entrada.next();
+
+                            System.out.println("Ingrese la cantidad: ");
+                            int cantidad5 = 0;
+                            cantidad5 = entrada.nextInt();
+
+                            System.out.println("Ingrese el precio: ");
+                            float precio = 0;
+                            precio = entrada.nextFloat();
+
+                            float resultado;
+                            resultado = precio * cantidad5;
+
+                            compra = compra + resultado;
+
+                            System.out.println("Producto: " + nombreproducto);
+                            System.out.println("Cantidad: " + cantidad5);
+                            System.out.println("Precio: " + precio);
+                            System.out.println("Subtotal: " + resultado);
+                            System.out.println("-----------------------------");
+
+                        }
+
+                        System.out.println("El total de la compra es: " + compra);
+
+                    }else{
+                        System.out.println("Ingrese solo cantidades positivas");
+                    }
+
                     break;
 
+
+
+
+
+
+
+
                 case 6:
-                    
+                    System.out.println("Calculo de area y perimetro de figuras");
+                    System.out.println("1- Cuadrado");
+                    System.out.println("2- Rectangulo");
+                    System.out.println("3- Triangulo");
+                    System.out.println("4- Circulo");
+                    System.out.println("5- Pentagono");
+
+                    int figura = 0;
+                    figura = entrada.nextInt();
+
+                    switch (figura) {
+                        case 1:
+                            System.out.println("Ingrese el lado del cuadrado:");
+                            double lado = entrada.nextDouble();
+
+                            double areaC = lado*lado;
+                            double perimetroC = lado * 4;
+
+                            System.out.println("Area: " + areaC);
+                            System.out.println("Perimetro: " + perimetroC);
+
+                            break;
+
+                            case 2:
+                                System.out.println("Ingrese base:");
+                                double base = entrada.nextDouble();
+
+                                System.out.println("Ingrese altura:");
+                                double altura = entrada.nextDouble();
+
+                                double areaR = base * altura;
+                                double perimetroR = 2 * (base + altura);
+
+                                System.out.println("Area: " + areaR);
+                                System.out.println("Perimetro: " + perimetroR);
+
+                            case 3:
+                                System.out.println("Ingrese base:");
+                                double baseT = entrada.nextDouble();
+
+                                System.out.println("Ingrese altura:");
+                                double alturaT = entrada.nextDouble();
+
+                                System.out.println("Ingrese lado 2:");
+                                double lado2 = entrada.nextDouble();
+
+                                System.out.println("Ingrese lado 3:");
+                                double lado3 = entrada.nextDouble();
+
+                                double areaT = (baseT * alturaT) / 2;
+                                double perimetroT = baseT + lado2 + lado3;
+
+                                System.out.println("Area: " + areaT);
+                                System.out.println("Perimetro: " + perimetroT);
+                                break;
+
+                            case 4:
+                                System.out.println("Ingrese el radio:");
+                                double radio = entrada.nextDouble();
+
+                                double areaCir = Math.PI * radio * radio;
+                                double perimetroCir = 2 * Math.PI * radio;
+
+                                System.out.println("Area: " + areaCir);
+                                System.out.println("Perimetro: " + perimetroCir);
+                                break;
+
+                            case 5:
+                                System.out.println("Ingrese el lado del pentagono:");
+                                double ladoP = entrada.nextDouble();
+
+                                System.out.println("Ingrese el apotema:");
+                                double apotema = entrada.nextDouble();
+
+                                double perimetroP = 5 * ladoP;
+                                double areaP = (perimetroP * apotema) / 2;
+
+                                System.out.println("Area: " + areaP);
+                                System.out.println("Perimetro: " + perimetroP);
+                                break;
+
+                        default:
+                            System.out.println("¡Figura no valida!  ");
+                    }
                     break;
 
                 case 7:
